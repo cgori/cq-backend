9c4f4da951edec6b17b7b9b4b6fb13a2a488b296
cgori <36439786+cgori@users.noreply.github.com>
2019-12-08 17:48:43 +0000

    poll change

--- src/models/poll.js
+++ src/models/poll.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 
 const poll = new mongoose.Schema({
-    poll: [
-        {
-            id: Number,
-            patient: { allergy: String, drugs: String, desc: String },
-            title: String,
-            description: String,
-            options: [{ title: String, votes: Number }],
-        },
-    ],
+    poll: {
+        id: Number,
+        patient: { allergy: String, drugs: String, desc: String },
+        title: String,
+        description: String,
+        options: [{ title: String, votes: Number }],
+    },
 });
 
 module.exports = mongoose.model('poll', poll, 'poll');
