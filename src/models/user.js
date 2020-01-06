94a1dc37f96066112bb13e641ca4412c8c080a40
cgori <99cgor@gmail.com>
2020-01-06 22:08:09 +0000

    pending

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, dropDups: true },
     password: { type: String, required: true },
     role: { type: String, required: true },
-    status: { type: String, required: true },
+    status: { type: String, required: true, default: 'pending' },
 });
 
 UserSchema.pre('save', function(next) {
